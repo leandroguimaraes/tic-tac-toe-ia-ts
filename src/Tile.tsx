f4f4ec267a8991dc724109834f106bca035663e1
Leandro Martins Guimarães <leandropl@gmail.com>
2022-11-02 12:19:03 -0300

    Props refactoring

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,26 +1,23 @@
 import { Move, MoveStatus } from "./lib/ticTacToe";
 import "./Tile.scss";
 
-function Tile(props: any) {
-  const move: Move = props.move;
-  const status: MoveStatus = props.status;
-
+function Tile(props: { move: Move; status: MoveStatus }) {
   return (
     <div
       className={`tile ${
-        status === MoveStatus.Win
+        props.status === MoveStatus.Win
           ? "win"
-          : status === MoveStatus.Loss
+          : props.status === MoveStatus.Loss
           ? "loss"
-          : status === MoveStatus.Draw
+          : props.status === MoveStatus.Draw
           ? "draw"
           : ""
-      } ${move === Move.null ? "empty" : ""}`}
+      } ${props.move === Move.null ? "empty" : ""}`}
     >
       <span className="material-symbols-outlined">
-        {move === Move.X
+        {props.move === Move.X
           ? "close"
-          : move === Move.O
+          : props.move === Move.O
           ? "radio_button_unchecked"
           : ""}
       </span>
